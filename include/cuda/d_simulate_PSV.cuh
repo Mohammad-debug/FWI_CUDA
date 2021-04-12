c7287015a88f99a51912e209071bffc934ce109a
mbbasnet <minbasnet@gmail.com>
2021-04-12 13:15:17 +0200

    first new cuda steps

--- include/cuda/d_simulate_PSV.cuh
+++ include/cuda/d_simulate_PSV.cuh
@@ -20,6 +20,21 @@
 //#include "d_PCG_PSV.hpp"
 */
 
+
+void g_simulate_PSV(int nt, int nz, int nx, real dt, real dz, real dx, 
+    int snap_z1, int snap_z2, int snap_x1, int snap_x2, int snap_dt, int snap_dz, int snap_dx, 
+    bool surf, bool pml_z, bool pml_x, int nsrc, int nrec, int nshot, int stf_type, int rtf_type, 
+    int fdorder, real scalar_lam, real scalar_mu, real scalar_rho,
+    real *&hc, int *&isurf, real **&lam, real **&mu, real **&rho, 
+    real *&a_z, real *&b_z, real *&K_z, real *&a_half_z, real *&b_half_z, real *&K_half_z,
+    real *&a_x, real *&b_x, real *&K_x, real *&a_half_x, real *&b_half_x, real *&K_half_x,
+    int *&z_src, int *&x_src, int *&z_rec, int *&x_rec,
+    int *&src_shot_to_fire, real **&stf_z, real **&stf_x, 
+    bool accu_save, bool seismo_save, 
+    real ***&rtf_z_true, real ***&rtf_x_true,
+    int mat_save_interval, int &taper_t1, int &taper_t2, int &taper_b1, int &taper_b2, 
+    int &taper_l1, int &taper_l2, int &taper_r1, int &taper_r2, int &fwinv);
+
 void simulate_fwd_PSV(int nt, int nz, int nx, real dt, real dz, real dx, 
     int snap_z1, int snap_z2, int snap_x1, int snap_x2, int snap_dt, int snap_dz, int snap_dx, 
     bool surf, bool pml_z, bool pml_x, int nsrc, int nrec, int nshot, int stf_type, int rtf_type, 
--- src/h_simulate_PSV.cpp
+++ src/h_simulate_PSV.cpp
@@ -55,7 +55,7 @@ real ***RTF_Z_TRUE, ***RTF_X_TRUE; // rtf (measured (true) values)
 bool RTF_MEAS_TRUE; // if the true measurement exists
 
 // simulation parameters
-bool h_FWINV; // 0.fwd, 1.fwu
+bool FWINV; // 0.fwd, 1.fwu
 int FDORDER, FPAD; // FD order and the numerical padding in each side
 real *HC; // Holberg coefficients
 bool ACCU_SAVE; // if to write accumulated tensor storage to disk
@@ -98,7 +98,7 @@ void simulate_PSV(){
     
     // Reading input integer parameters
     std::cout << "Reading input parameters <INT>."<<std::endl;
-    read_input_metaint(GPU_CODE, SURF, PML_Z, PML_X, ACCU_SAVE, SEISMO_SAVE, h_FWINV, RTF_MEAS_TRUE, NT, NZ, NX, 
+    read_input_metaint(GPU_CODE, SURF, PML_Z, PML_X, ACCU_SAVE, SEISMO_SAVE, FWINV, RTF_MEAS_TRUE, NT, NZ, NX, 
     SNAP_T1, SNAP_T2, SNAP_Z1, SNAP_Z2, SNAP_X1, SNAP_X2, SNAP_DT, SNAP_DZ, SNAP_DX,
     TAPER_T1, TAPER_T2, TAPER_B1, TAPER_B2, TAPER_L1, TAPER_L2, TAPER_R1, TAPER_R2,
     NSRC, NREC, NSHOT, STF_TYPE, RTF_TYPE, FDORDER, FPAD, MAT_SAVE_INTERVAL, MAT_GRID); 
@@ -116,7 +116,7 @@ void simulate_PSV(){
     std::cout << "Reading input parameters <FLOAT>."<<std::endl;
     read_inp_metafloat(DT, DZ, DX, h_NPOWER, h_DAMP_V_PML, h_RCOEF, h_K_MAX_PML, h_FREQ_PML, SCALAR_LAM, SCALAR_MU, SCALAR_RHO); 
 
-    if(h_FWINV){
+    if(FWINV){
         if(RTF_MEAS_TRUE){
             std::cout << "Reading field measurements <FLOAT>."<<std::endl;
             for(int ishot=0;ishot<NSHOT;ishot++){
@@ -194,18 +194,16 @@ void simulate_PSV(){
         // Calling now the device codes
         std::cout << "THE COMPUTATION STARTS IN GPU" << std::endl;
 
-        if (h_FWINV){
 
-            // Full Waveform Inversion
-            std::cout << "Full Waveform Inversion (GPU) ...."<<std::endl;
             //
             /*
             g_simulate_fwi_PSV(NT, NZ, NX, DT, DZ, DX, SNAP_Z1, SNAP_Z2, SNAP_X1, SNAP_X2, SNAP_DT, SNAP_DZ, SNAP_DX,
             SURF, PML_Z, PML_X, NSRC, NREC, NSHOT, STF_TYPE, RTF_TYPE, FDORDER, SCALAR_LAM, SCALAR_MU, SCALAR_RHO,
             HC, ISURF, LAM, MU, RHO, A_Z, B_Z, K_Z, A_HALF_Z, B_HALF_Z, K_HALF_Z, A_X, B_X, K_X, A_HALF_X, B_HALF_X,
-            K_HALF_X, Z_SRC, X_SRC, Z_REC, X_REC, SRC_SHOT_TO_FIRE, STF_Z, STF_X, 
+            K_HALF_X, Z_SRC, X_SRC, Z_REC, X_REC, SRC_SHOT_TO_FIRE, STF_Z, STF_X,
+            ACCU_SAVE, SEISMO_SAVE,
             RTF_Z_TRUE, RTF_X_TRUE, MAT_SAVE_INTERVAL, TAPER_T1, TAPER_T2, TAPER_B1, TAPER_B2, 
-            TAPER_L1, TAPER_L2, TAPER_R1, TAPER_R2);
+            TAPER_L1, TAPER_L2, TAPER_R1, TAPER_R2, FWINV);
             */
 
         }
@@ -233,7 +231,7 @@ void simulate_PSV(){
         // Calling the codes in the host only
         std::cout << "THE COMPUTATION STARTS IN CPU" << std::endl;
         
-        if (h_FWINV){
+        if (FWINV){
             // Full Waveform Inversion
             std::cout << "Full Waveform Inversion...."<<std::endl;
             //
