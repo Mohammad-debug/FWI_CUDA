c03744541dd7051ea1141c03bde0e2c6be9cc69b
mbbasnet <minbasnet@gmail.com>
2022-06-10 11:41:16 +0200

    .

--- scripts/pre_proc_dam_crack.py
+++ scripts/pre_proc_dam_crack.py
@@ -61,10 +61,11 @@ pml_z = True; pml_x = True # PML exist in both direction
 npml_top = 20; npml_bottom = 20; npml_left = 20; npml_right = 20
 
 # Geometric data
-dt = 1.85e-5; dz = 0.01; dx = 0.01; # grid intervals
-nt = 10000.0; 
-nz = fpad + npml_top + int(dep/dz) + npml_bottom + fpad+ 1
-nx = fpad + npml_left + int(len/dx) + npml_right + fpad + 1 # grid numbers (adding for PMLs as well)
+dt = 0.5e-5; dz = 0.02; dx = 0.02; # grid intervals
+nt = np.int32(0.1/0.5e-5); 
+print('nt: ', nt)
+nz = fpad + npml_top + np.int32(dep/dz) + npml_bottom + fpad+ 1
+nx = fpad + npml_left + np.int32(len/dx) + npml_right + fpad + 1 # grid numbers (adding for PMLs as well)
 
 
 # Surface grid index in each direction (0 = no surface)
@@ -74,11 +75,11 @@ isurf_top = 0; isurf_bottom = 0; isurf_left = 0; isurf_right = 0
 
 
 snap_t1 = 0; snap_t2 = nt-1 # snap in time steps
-snap_z1 = fpad + npml_top #+int(d_wt/dz) 
-snap_z2 = nz - fpad - npml_bottom #fpad+npml_top+int(d_tot/dz) # snap boundaries z
-snap_x1 = fpad + npml_left #int(nx/2 - 1.0*l_top/dz) 
-snap_x2 = nx - fpad - npml_right #int(nx/2  + 1.0*l_top/dz) # snap boundaries x
-snap_dt = 10; snap_dz = 1; snap_dx = 1; # the snap interval
+snap_z1 = fpad + npml_top #+np.int32(d_wt/dz) 
+snap_z2 = nz - fpad - npml_bottom #fpad+npml_top+np.int32(d_tot/dz) # snap boundaries z
+snap_x1 = fpad + npml_left #np.int32(nx/2 - 1.0*l_top/dz) 
+snap_x2 = nx - fpad - npml_right #np.int32(nx/2  + 1.0*l_top/dz) # snap boundaries x
+snap_dt = 20; snap_dz = 1; snap_dx = 1; # the snap interval
 
 # Taper position
 nz_snap = snap_z2 - snap_z1
@@ -86,11 +87,11 @@ nx_snap = snap_x2 - snap_x1
 
 # taper relative to the total grid
 # t: top, b: bottom, l: left, r: right
-taper_t1 = snap_z1 + int(nz_snap*0.00); taper_t2 = taper_t1 + int(nz_snap*0.00)
-taper_b1 = snap_z2 - int(nz_snap*0.00); taper_b2 = taper_b1 - int(nz_snap*0.00)
+taper_t1 = snap_z1 + np.int32(nz_snap*0.00); taper_t2 = taper_t1 + np.int32(nz_snap*0.00)
+taper_b1 = snap_z2 - np.int32(nz_snap*0.00); taper_b2 = taper_b1 - np.int32(nz_snap*0.00)
 
-taper_l1 = snap_x1 + int(nx_snap*0.0); taper_l2 = taper_l1 + int(nx_snap*0.00)
-taper_r1 = snap_x2 - int(nx_snap*0.0); taper_r2 = taper_r1 - int(nx_snap*0.00)
+taper_l1 = snap_x1 + np.int32(nx_snap*0.0); taper_l2 = taper_l1 + np.int32(nx_snap*0.00)
+taper_r1 = snap_x2 - np.int32(nx_snap*0.0); taper_r2 = taper_r1 - np.int32(nx_snap*0.00)
 #snap_z1 = 1; snap_z2 = nz-1  # snap boundaries z
 #snap_x1 = 1; snap_x2 = nx-1 # snap boundaries x
 
@@ -150,9 +151,9 @@ rho = np.full((nz, nx), rho_air)
 for iz in range(0, nz):
     for ix in range(0, nx):
         # for sand dam
-        if (iz>int(fpad + npml_top+d_top/dz)): # top boundary 
-            if (iz - int(fpad + npml_top+d_top/dz) >= 0.33*(ix - (fpad + npml_left + int((l_uadd + l_usl + l_top)/dx)))):
-                if (iz - (fpad + npml_top + int(d_top/dz)) >= -0.33*(ix - (fpad + npml_left + int((l_uadd + l_usl)/dx)))):
+        if (iz>np.int32(fpad + npml_top+d_top/dz)): # top boundary 
+            if (iz - np.int32(fpad + npml_top+d_top/dz) >= 0.33*(ix - (fpad + npml_left + np.int32((l_uadd + l_usl + l_top)/dx)))):
+                if (iz - (fpad + npml_top + np.int32(d_top/dz)) >= -0.33*(ix - (fpad + npml_left + np.int32((l_uadd + l_usl)/dx)))):
                     lam[iz][ix] = lam_sand
                     mu[iz][ix] = mu_sand
                     rho[iz][ix] = rho_sand
@@ -165,13 +166,13 @@ for iz in range(0, nz):
             
                     
                     
-        if (iz>int(fpad + npml_top+d_wt/dz)): # water level boundary
-            if (iz - (fpad + npml_top + int(d_top/dz)) < -0.33*(ix - (fpad + npml_left + int((l_uadd + l_usl)/dx)))):
+        if (iz>np.int32(fpad + npml_top+d_wt/dz)): # water level boundary
+            if (iz - (fpad + npml_top + np.int32(d_top/dz)) < -0.33*(ix - (fpad + npml_left + np.int32((l_uadd + l_usl)/dx)))):
                 lam[iz][ix] = lam_water
                 mu[iz][ix] = mu_water
                 rho[iz][ix] = rho_water
                 
-        if (iz>int(fpad + npml_top +d_sub/dz)): # subsurface boundary
+        if (iz>np.int32(fpad + npml_top +d_sub/dz)): # subsurface boundary
             lam[iz][ix] = lam_sub
             mu[iz][ix] = mu_sub
             rho[iz][ix] = rho_sub
@@ -206,7 +207,7 @@ pml_npower_pml = 2.0
 damp_v_pml = Cp
 rcoef = 0.001
 k_max_pml = 1.0
-freq_pml = 200.0 # PML frequency in Hz
+freq_pml = 800.0 # PML frequency in Hz
 
 # -----------------------------------------------------
 
@@ -221,54 +222,54 @@ freq_pml = 200.0 # PML frequency in Hz
 stf_type = 1; rtf_type = 0 # 1:velocity, 2:displacement
 
 # Creating source locations
-#xsrc = np.array([10 + int(1.0/dx), 10 + int(4.0/dx), 10 + int(7.0/dx)], dtype=int)
-#xsrc = np.array([npml_left + int((4.0)/dx)], dtype=int)
-#zsrc = np.full((xsrc.size,), npml_top+ int((d_wt+0.5)/dz), dtype=int)
+#xsrc = np.array([10 + np.int32(1.0/dx), 10 + np.int32(4.0/dx), 10 + np.int32(7.0/dx)], dtype=np.int32)
+#xsrc = np.array([npml_left + np.int32((4.0)/dx)], dtype=np.int32)
+#zsrc = np.full((xsrc.size,), npml_top+ np.int32((d_wt+0.5)/dz), dtype=np.int32)
 
 # Creating source locations
-xsrc = np.zeros((3,), dtype=int)
-zsrc = np.zeros((3,), dtype=int)
+xsrc = np.zeros((3,), dtype=np.int32)
+zsrc = np.zeros((3,), dtype=np.int32)
 nsrc = zsrc.size # counting number of sources from the source location data
 
 for isr in range(0,zsrc.size):
     ix = (12.0)/nsrc
     iz = (4.0)/nsrc
-    xsrc[isr] = int(fpad + npml_left + (l_uadd + l_usl - isr*ix + 0.1)/dx)
-    zsrc[isr] = int(fpad + npml_top + (d_top +isr*iz+0.1)/dz)
+    xsrc[isr] = np.int32(fpad + npml_left + (l_uadd + l_usl - isr*ix + 0.1)/dx)
+    zsrc[isr] = np.int32(fpad + npml_top + (d_top +isr*iz+0.1)/dz)
 
 
 
 # Creating source to fire arrays
-src_shot_to_fire = np.arange(0,nsrc,1, dtype=int)
-#src_shot_to_fire = np.zeros((nsrc,), dtype=int)
+src_shot_to_fire = np.arange(0,nsrc,1, dtype=np.int32)
+#src_shot_to_fire = np.zeros((nsrc,), dtype=np.int32)
 
 nshot = nsrc # fire each shot separately
 
 '''
 # Creating reciever locations
-xrec1 = np.zeros((24,), dtype=int)
-zrec1 = np.zeros((24,), dtype=int)
+xrec1 = np.zeros((24,), dtype=np.int32)
+zrec1 = np.zeros((24,), dtype=np.int32)
 nrec = zrec.size # counting the recievers from the locations
 
 for ir in range(0,zrec.size):
     ix = (12.0)/nrec
     iz = (4.0)/nrec
-    xrec[ir] = int(fpad + npml_left + (l_uadd + l_usl + l_top + ir*ix - 0.1)/dx)
-    zrec[ir] = int(fpad + npml_top + (d_top +ir*iz+0.1)/dz)
+    xrec[ir] = np.int32(fpad + npml_left + (l_uadd + l_usl + l_top + ir*ix - 0.1)/dx)
+    zrec[ir] = np.int32(fpad + npml_top + (d_top +ir*iz+0.1)/dz)
 '''    
-xrec = np.linspace(int(fpad + npml_left + (l_uadd )/dx), int(fpad + npml_left +(l_uadd +l_usl + l_top+l_dsl)/dx), 55, dtype=int)
+xrec = np.linspace(np.int32(fpad + npml_left + (l_uadd )/dx), np.int32(fpad + npml_left +(l_uadd +l_usl + l_top+l_dsl)/dx), 55, dtype=np.int32)
 nrec = xrec.size # counting the recievers from the locations 
-zrec = np.zeros(xrec.size)  
+zrec = np.zeros(xrec.size, dtype=np.int32)  
 # plotting the middle line 
 x_mid = (2*fpad+npml_left +npml_right+(l_uadd+l_usl+l_top+l_dsl+l_dadd)/dx + 1)/2
 slope_factor = (1/3)#* (l_usl + l_top+l_dsl)/(dx*nrec)
 for ii in range(0, xrec.size):
-    if xrec[ii] < int(fpad + npml_left + (l_uadd + l_usl )/dx):
-        zrec[ii] = int(fpad + npml_top + (d_sub+0.1)/dz - 0.33*(xrec[ii]-xrec[0]))
-        #zrec[-ii+1] = int(fpad + npml_top + d_sub/dz - 0.33*(xrec[ii]-xrec[0]))
-    elif xrec[ii] < int(fpad + npml_left + (l_uadd + l_usl + l_top )/dx):
-        zrec[ii]    = int(fpad + npml_top + (d_top+0.1)/dz)
-        #zrec[-ii+1] = int(fpad + npml_top + (d_top+0.1)/dz)
+    if xrec[ii] < np.int32(fpad + npml_left + (l_uadd + l_usl )/dx):
+        zrec[ii] = np.int32(fpad + npml_top + (d_sub+0.1)/dz - 0.33*(xrec[ii]-xrec[0]))
+        #zrec[-ii+1] = np.int32(fpad + npml_top + d_sub/dz - 0.33*(xrec[ii]-xrec[0]))
+    elif xrec[ii] < np.int32(fpad + npml_left + (l_uadd + l_usl + l_top )/dx):
+        zrec[ii]    = np.int32(fpad + npml_top + (d_top+0.1)/dz)
+        #zrec[-ii+1] = np.int32(fpad + npml_top + (d_top+0.1)/dz)
     else:
         zrec[ii] = zrec[-ii-1]
         
@@ -337,11 +338,11 @@ metabool = np.array([cuda_computation, surf, pml_z, pml_x, accu_save, seismo_sav
 # Creating integer arrays and subsequent concatenation of the related fields
 metaint = np.array([nt, nz, nx, snap_t1, snap_t2, snap_z1, snap_z2, snap_x1, snap_x2, snap_dt, snap_dz, snap_dx, \
                     taper_t1, taper_t2, taper_b1, taper_b2, taper_l1, taper_l2, taper_r1, taper_r2,\
-                    nsrc, nrec, nshot, stf_type, rtf_type, fdorder, fpad, mat_save_interval, mat_grid], dtype=int)
+                    nsrc, nrec, nshot, stf_type, rtf_type, fdorder, fpad, mat_save_interval, mat_grid], dtype=np.int32)
 metaint = np.concatenate((metabool, metaint), axis=None) # concatination of boolen and integer as integers
 
 # additional concatenation of int arrays
-intarray = np.array([npml_top, npml_bottom, npml_left, npml_right, isurf_top, isurf_bottom, isurf_left, isurf_right], dtype=int)
+intarray = np.array([npml_top, npml_bottom, npml_left, npml_right, isurf_top, isurf_bottom, isurf_left, isurf_right], dtype=np.int32)
 intarray  = np.concatenate((intarray, zsrc), axis=None)
 intarray  = np.concatenate((intarray, xsrc), axis=None)
 intarray  = np.concatenate((intarray, src_shot_to_fire), axis=None)
@@ -349,6 +350,7 @@ intarray  = np.concatenate((intarray, zrec), axis=None)
 intarray  = np.concatenate((intarray, xrec), axis=None)
 
 print("Metaint: ", metaint)
+print("Int array: ", intarray)
 
 # Creating float arrays and subsequent concatenation
 metafloat = np.array([dt, dz, dx, pml_npower_pml, damp_v_pml, rcoef, k_max_pml, freq_pml, \
